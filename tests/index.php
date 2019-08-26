42b4959c2f158dacebbcba89f124d99086c6c084
Dede Gunawan <dede@unsil.ac.id>
2019-08-26 11:34:37 +0700

    initialize data

---
+++ tests/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tik_squad
+ * Date: 8/26/19
+ * Time: 11:31 AM
+ */
+
+require_once '../vendor/autoload.php';
+
+$data = new \DedeGunawan\UtilityClass\DataStructure([]);
+
+var_dump($data);die();
